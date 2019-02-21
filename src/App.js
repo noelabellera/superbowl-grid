71fa9b5d69aa979a88fa25d592154b98af3a4d30
Noel Abellera <noel.abellera@gmail.com>
2019-02-20 23:54:33 -0800

    add form for event name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      afcTeam: "",
+      nfcTeam:  "",
+      event: ""
+    }
+
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Grid Generator for Sports</h1>
+
+        <h3>Name of Event:</h3>
+        <form>
+          <label>
+            <input type="text"/>
+          </label>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
